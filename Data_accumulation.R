5c96dac0e905f8b1f183488e14007f3c4cbe2669
Mateusz Stachnik <msstachnik@gmail.com>
2017-07-01 10:55:00 +0200

    Data analysis

--- Data_accumulation.R
+++ Data_accumulation.R
@@ -1,9 +1,14 @@
 # Accumulation fit data
 
-load('vp_list.RData')
+load('ols_fit.RData')
 v1 = vp_out;
 load('orth_nonlin_ls_fit.RData')
 v2 = vp_out;
-vp_out = rbind.data.frame(v1, v2)
+load('odr_ls_fit.RData')
+v3 = vp_out;
+load('odr_without_jac_ls_fit.RData')
+v4 = vp_out;
+vp_out = plyr::rbind.fill(v1, v2, v3, v4)
+
 
 save(vp_out, file = "fit_accumulated.RData")
